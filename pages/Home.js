b917fe06675ea10090e4a53d45ec176dbd6e4d6e
ilse31 <ilhamprazsetyo33@gmail.com>
2022-07-30 00:24:31 +0700

    add new Home Pages

--- pages/Home.js
+++ pages/Home.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { IconApp } from "../assets/icons";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+          left: -20,
+        }}>
+        <Image source={IconApp} />
+        <Text style={styles.TitlePage}>Whatshy</Text>
+      </View>
+      <View>
+        <Text>Home</Text>
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 35,
+  },
+  TitlePage: {
+    fontFamily: "Poppins_100ExtraLight",
+    fontSize: 36,
+    marginLeft: 15,
+    alignSelf: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+});
