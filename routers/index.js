3053c4a227956244840445b384291af448ad1acd
ilse31 <ilhamprazsetyo33@gmail.com>
2022-07-30 12:01:10 +0700

    new tab component

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,58 @@ const { createStackNavigator } = require("@react-navigation/stack");
 import { NavigationContainer } from "@react-navigation/native";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import History from "../pages/History";
+import Account from "../pages/Account";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image } from "react-native";
+import HomeIcon from "../assets/icons/HomeIcon.png";
+import ProfileIcon from "../assets/icons/profileIcon.png";
+import HistoryIcon from "../assets/icons/historyIcon.png";
+
+const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#00D7B9",
+          height: 55,
+        },
+      }}>
+      <Tab.Screen
+        name='Home'
+        component={Home}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: () => <Image source={HomeIcon} />,
+        }}
+      />
+      <Tab.Screen
+        name='History'
+        component={History}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: () => (
+            <Image source={HistoryIcon} style={{ width: 30, height: 30 }} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Account'
+        component={Account}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: () => (
+            <Image source={ProfileIcon} style={{ width: 30, height: 30 }} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const StackRoute = () => {
   return (
@@ -14,8 +65,8 @@ const StackRoute = () => {
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='Home'
-          component={Home}
+          name='MainApp'
+          component={MainApp}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
