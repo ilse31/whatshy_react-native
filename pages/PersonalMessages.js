a28f279c8fcfd87e45214a459366a2cc80588551
ilse31 <ilhamprazsetyo33@gmail.com>
2022-07-31 14:34:16 +0700

    new pages

---
+++ pages/PersonalMessages.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PersonalMessages = () => {
+  return (
+    <View>
+      <Text>PersonalMessages</Text>
+    </View>
+  );
+};
+
+export default PersonalMessages;
+
+const styles = StyleSheet.create({});
