5a758a833f32d73aed6d181adc67d60d3b188b1f
ilse31 <ilhamprazsetyo33@gmail.com>
2022-07-31 14:33:16 +0700

    add new pages

---
+++ pages/Broadcast.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Broadcast = () => {
+  return (
+    <View>
+      <Text>Broadcast</Text>
+    </View>
+  );
+};
+
+export default Broadcast;
+
+const styles = StyleSheet.create({});
